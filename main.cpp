b9975b036f32927099c713e9081c73752f152eaa
schbalazs <73988271+schbalazs@users.noreply.github.com>
2022-09-13 00:15:07 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N után _
-    std::cout << '1-100 ertekek duplazasa'  //; hiányzik illetve "" használat
-    for (int i = 0;)  //for ciklus hiányos és hibás
+    int* b= new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa \n";
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //for ciklus hibás
+    for (int i = 1; i <= N_ELEMENTS ; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << b[i] << "\n" ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
-        atlag += b[i]  //; hiányzik
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
